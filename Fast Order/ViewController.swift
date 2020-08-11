3d73c79f0b735ebd984b01803a39eba6c274be18
bera <domagojberonic1995@gmail.com>
2020-08-11 13:25:22 +0200

    Initial Commit

---
+++ Fast Order/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fast Order
+//
+//  Created by Domagoj Beronic on 11/08/2020.
+//  Copyright © 2020 Domagoj Beronic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
